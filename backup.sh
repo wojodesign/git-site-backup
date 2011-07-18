b600354d65391903125620a2809f047cc5208790
Brett Wejrowski <brett@thewojogroup.com>
2011-07-18 15:51:17 -0400

    updated... muuuuch cooler

--- backup.sh
+++ backup.sh
@@ -1,72 +1,141 @@
   #!/bin/bash
 
   #######################################################
-  # git-sitebackup v0.1
+  # git-sitebackup 
   #######################################################
-
- 
-
-  #######################################################
-  # Configuration
-  #######################################################
- 
-  # Database connection information
-  dbname=$1      # (e.g.: $dbname = "drupaldb")
-  dbuser=$2      # (e.g.: $dbuser = "drupaluser")
-  dbpass=$3      # (e.g.: $dbpass = "drupaldbPassword")
- 
-  # Website Files
-  webrootdir=$4  # (e.g.: $webrootdir = "/home/user/public_html")
-
+  version = "0.1"
  
   #######################################################
   # Banner
   #######################################################
-  echo -e "\n VMARK-sitebackup v0.1\n"
- 
+  echo -e "\n git-sitebackup $version \n"
+
+  #######################################################
+  # Grand Central... direct to the correct actions
+  # based on number of inputs
+  #######################################################
+
+
+  # only filepath present: do not backup dB and do not push to remote
+  if[$# -eq 1 ]; then
+	
+   
+	
+	  dir = $1
+	  commit_changes $dir
+	
+	
+	
+  # only filepath & remote branch present: do not backup dB, push to remote
+  elif[$# -eq 2 ]; then
+	
+	dir = $1
+	branch = $2
+	
+	commit_changes $dir
+	push_to_remote $dir $branch
+	
+	
+  # filepath and dB info present: backup both, do not push to remote
+  elif[$# -eq 4 ]; then
+	
+	dir = $1
+	dbname = $2
+	dbuser = $3
+	dbpass = $4
+	
+	dump_db $dir $dbname $dbuser $dbpass
+	commit_changes $dir 
+		
+  # all arguments present: backup files and db, push to remote
+  elif[$# -eq 5 ]; then
+    dir = $1
+	dbname = $2
+	dbuser = $3
+	dbpass = $4
+	branch = $5
+
+	dump_db $dir $dbname $dbuser $dbpass
+	commit_changes $dir 
+	push_to_remote $dir $branch
+
+  fi
+	
+  #cleanup and exit
+  cleanup
+	
+	
  
   #######################################################
   # sqldump database information
   #######################################################
-  echo -e " .. sqldump'ing database:"
-  echo -e "    user: $dbuser database: $dbname"
-  cd $webrootdir
-  mysqldump --password=$dbpass --user=$dbuser --skip-extended-insert $dbname > dbcontent.sql  
-  echo -e "    done\n"
- 
- 
- 
+  function dump_db {
+    dir = $1
+	dbname = $2
+	dbuser = $3
+	dbpass = $4
+   
+    echo -e " .. sqldump'ing database:"
+    echo -e "    user: $dbuser database: $dbname"
+    cd $dir
+    mysqldump --password=$dbpass --user=$dbuser --skip-extended-insert $dbname > dbcontent.sql  
+    echo -e "    done\n"
+  }
+
+
+
   #######################################################
   # Commit to Git Repo
   #######################################################
-  echo -e " .. Committing to local git repository at $gitRepoBase"
-  datestamp=$(date +"%Y-%m-%d")
-  timestamp=$(date +"%H:%M")
-  cd $webrootdir
-  git add *
-  git commit -am "Backup created on $datestamp at $timestamp."
-  echo -e "    done\n"
- 
- 
+  function commit_changes {
+    dir = $1
+
+    echo -e " .. Committing to local git repository at $dir"
+	datestamp=$(date +"%Y-%m-%d")
+	timestamp=$(date +"%H:%M")
+	cd $dir
+	git add *
+	git commit -am "Backup created on $datestamp at $timestamp."
+	echo -e "    done\n"
+  }
+
+
+
+
   #######################################################
   # Push out to Master
   #######################################################
-  echo -e " .. Pushing backup to Master repo"
-  git push origin master
-  echo -e "    done\n"
+  function push_to_remote {
+    dir = $1
+    branch = $2
+
+    echo -e " .. Pushing backup to $2 repo"
+	git push origin $2
+	echo -e "    done\n"
+	
+  }
 
 
- 
   #######################################################
   # Cleanup
   #######################################################
-  echo -e " .. Cleaning up"
-  git gc
-  echo -e "    done\n"
+  function cleanup {
+    echo -e " .. Cleaning up"
+    git gc
+    echo -e "    done\n"
 
 
+	# Exit banner
+	echo -e " .. Full site backup complete\n"
+  }
+
+  
+
+  #######################################################
+  # Install 
+  #######################################################  
+  function install {
+
+  }
  
-  #######################################################
-  # Exit banner
-  #######################################################
-  echo -e " .. Full site backup complete\n"
+ 
